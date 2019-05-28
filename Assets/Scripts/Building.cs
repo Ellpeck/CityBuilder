60d9decda48023d6bd205695e7065fa08f8c663a
Ellpeck <megamaximal@gmail.com>
2019-05-28 19:38:06 +0200

    placing objects

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    private static readonly int Ghost = Animator.StringToHash("Ghost");
+    private static readonly int PlaceHere = Animator.StringToHash("CanPlaceHere");
+
+    public LayerMask disallowedLayers;
+
+    private Animator anim;
+    private Rigidbody2D body;
+    private new BoxCollider2D collider;
+
+    private void Awake() {
+        this.anim = this.GetComponent<Animator>();
+        this.body = this.GetComponent<Rigidbody2D>();
+        this.collider = this.GetComponent<BoxCollider2D>();
+    }
+
+    public void SetGhost() {
+        this.body.bodyType = RigidbodyType2D.Dynamic;
+        this.anim.SetBool(Ghost, true);
+    }
+
+    public void Move(Vector3 position) {
+        this.transform.position = position;
+        this.anim.SetBool(PlaceHere, this.CanPlaceHere());
+    }
+
+    public bool CanPlaceHere() {
+        return !this.collider.IsTouchingLayers(this.disallowedLayers);
+    }
+
+}
---
+++ Assets/Scripts/PlacementManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlacementManager : MonoBehaviour {
+
+    public Building placingObject;
+
+    private Camera mainCamera;
+
+    private Building currentGhost;
+
+    private void Start() {
+        this.mainCamera = Camera.main;
+    }
+
+    private void Update() {
+        if (this.placingObject) {
+            var mousePos = Input.mousePosition;
+            if (mousePos.x < 0 || mousePos.y < 0 || mousePos.x >= this.mainCamera.pixelWidth || mousePos.y >= this.mainCamera.pixelHeight)
+                return;
+
+            var worldPos = this.mainCamera.ScreenToWorldPoint(mousePos);
+            var snappedWorldPos = new Vector3(Mathf.Ceil(worldPos.x - 0.5F), Mathf.Floor(worldPos.y + 0.5F), 0);
+
+            if (!this.currentGhost) {
+                this.currentGhost = Instantiate(this.placingObject, snappedWorldPos, Quaternion.identity);
+                this.currentGhost.SetGhost();
+            }
+            this.currentGhost.Move(snappedWorldPos);
+
+            if (Input.GetMouseButtonDown(0) && this.currentGhost.CanPlaceHere()) {
+                Instantiate(this.placingObject, snappedWorldPos, Quaternion.identity);
+                Destroy(this.currentGhost.gameObject);
+                this.currentGhost = null;
+            }
+        }
+    }
+
+}
